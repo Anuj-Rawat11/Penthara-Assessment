8d4c648178d63943d7612b47cdc9024de63e7bad
Anuj-Rawat11 <8909rawatanuj@gmail.com>
2026-09-05 08:28:22 +0530

    feat: enhance MongoDB connection handling with error logging and process exit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose");
 const dns = require("dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 async function main() {
-  await mongoose.connect(process.env.MONGO_URI);
+  try{
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  }
+  catch(err){
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the process with an error code
+  }
+  
 }
 
 module.exports = main;
